983acea6a908c167532916b49e4b96aa923d1ddd
Favor Vincent <vfavorm77@gmail.com>
2024-07-10 19:40:57 +0300

    Check session complete

--- server/app.py
+++ server/app.py
@@ -2,17 +2,46 @@
 
 # Standard library imports
 
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 
 # Remote library imports
 from flask import request, jsonify, session, make_response
 from flask_restful import Resource
 
 # Local imports
-from config import app, db, api
+from config import app, db, api, bcrypt
 # Add your model imports
-
 from models import User
+
 # Views go here!
+@app.route('/signup', methods=['POST'])
+def sign_up():
+    data = request.get_json()
+    name = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+    balance = data.get('balance')
+
+    if not name or not email or not password or not balance:
+        return jsonify({"error": "All fields are required"}), 422
+    
+    new_user = User(
+        name=name,
+        email=email,
+        password=password,
+        balance=balance,
+    )
+
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({"message": "User created successfully"}), 201
+    except IntegrityError:
+        db.session.rollback()
+        return jsonify({"error": "Email already exists"}), 422
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": "Failed to create user", "details": str(e)}), 500
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -21,15 +50,15 @@ def login():
     password = data.get('password')
     user = User.query.filter_by(email=email).first()
     
-    if user and user.password == password:
+    if user and user.authenticate(password):
         session['user_id'] = user.id
         return jsonify({'success': True})
     
     return jsonify({'success': False, 'message': 'Invalid email or password'}), 401
 
 @app.route('/checksession', methods=['GET'])
-def check_session(self):
-    user_id = session['user_id']
+def check_session():
+    user_id = session.get('user_id')
     if user_id:
         user = User.query.filter(User.id == user_id).first()
         return make_response(user.to_dict(), 200)
--- server/migrations/versions/51d245dff977_initial_migration.py
+++ server/migrations/versions/51d245dff977_initial_migration.py
@@ -1,8 +1,8 @@
-"""initial migration
+""" initial migration
 
-Revision ID: 791c9adaea0c
+Revision ID: 51d245dff977
 Revises: 
-Create Date: 2024-07-10 18:01:32.726050
+Create Date: 2024-07-10 19:26:37.909279
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '791c9adaea0c'
+revision = '51d245dff977'
 down_revision = None
 branch_labels = None
 depends_on = None
